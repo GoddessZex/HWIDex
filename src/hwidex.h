f78ab32182b6699ba265c5a63d1afa2f06837a3e
Zex <goddessszex@gmail.com>
2023-10-31 22:43:35 +0100

    Fixed kernel's spaghetti code :clueless:

--- src/hwidex.h
+++ src/hwidex.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstdint>
 #include <string>
 #include <sstream>
@@ -12,6 +14,10 @@
 #include <intrin.h>
 
 /**
+ * ===== HWIDex created by Zex and Kernel =====
+ */
+
+/**
  * ===== STAGES =====
  * 1. create functions for vendor, brand, thread count, BIOS serial, etc...
  * 2. invoke all of them into a std::vector
@@ -19,179 +25,151 @@
  * 4. return it as a 20-character std::string
  */
 
-std::string GetHWID() {
-    using i32 = std::int32_t;
-    using u8 = std::uint8_t;
-    using u16 = std::uint16_t;
-    using u32 = std::uint32_t;
-    using u64 = std::uint64_t;
+ // check for cpuid presence and accessibility
+bool CheckCpuid() noexcept {
+    int info[4];
+    __cpuid(info, 0);
+    return (info[0] >= 1);
+}
 
-    // check for cpuid presence and accessibility
-    auto check_cpuid = []() noexcept -> bool {
-#if ( \
-            !defined(__x86_64__) && \
-            !defined(__i386__) && \
-            !defined(_M_IX86) && \
-            !defined(_M_X64) \
-        )
-        return false;
-#endif
+// fetch CPU brand
+std::string GetCpuBrand() {
+    using u32 = unsigned int;
+    std::stringstream ss;
 
-        i32 info[4];
-        __cpuid(info, 0);
-        return (info[0] >= 1);
-        };
-
-    // cpuid wrapper for MSVC
-    auto cpuid = []
-    (
-        u32& a, u32& b, u32& c, u32& d,
-        const u32 a_leaf,
-        const u32 c_leaf = 0xFF  // default dummy value
-        ) noexcept -> void {
-            i32 x[4];
-            __cpuidex((i32*)x, a_leaf, c_leaf);
+    if (CheckCpuid()) {
+        auto cpuid = [](u32& a, u32& b, u32& c, u32& d, const u32 a_leaf, const u32 c_leaf = 0xFF) {
+            int x[4];
+            __cpuidex(x, a_leaf, c_leaf);
             a = static_cast<u32>(x[0]);
             b = static_cast<u32>(x[1]);
             c = static_cast<u32>(x[2]);
             d = static_cast<u32>(x[3]);
-        };
-
-    // get cpu brand (e.g. GenuineIntel, AuthenticAMD, etc...)
-     auto brand = [&]() -> std::string {
-        if (!check_cpuid()) {
-            return "";
-        }
-
-        auto cpuid_thingy = [&](const u32 p_leaf, u32* regs, std::size_t start = 0, std::size_t end = 4) -> bool {
-            u32 x[4];
-            cpuid(x[0], x[1], x[2], x[3], p_leaf);
-
-            for (; start < end; start++) {
-                *regs++ = x[start];
-            }
-
-            return true;
             };
 
         u32 sig_reg[3] = { 0 };
 
-        if (!cpuid_thingy(0, sig_reg, 1)) {
-            return "";
+        if (sig_reg[0] >= 1) {
+            u32 features;
+            cpuid(features, sig_reg[1], sig_reg[2], sig_reg[3], 1, 2);
+
+            auto strconvert = [](unsigned long long n) -> std::string {
+                const std::string& str(reinterpret_cast<char*>(&n));
+                return str;
+                };
+
+            ss << strconvert(sig_reg[0]);
+            ss << strconvert(sig_reg[2]);
+            ss << strconvert(sig_reg[1]);
         }
+    }
 
-        u32 features;
-        cpuid_thingy(1, &features, 2, 3);
+    return ss.str();
+}
 
-        auto strconvert = [](u64 n) -> std::string {
-            const std::string& str(reinterpret_cast<char*>(&n));
-            return str;
+// fetch CPU vendor
+std::string GetCpuVendor() {
+    using u32 = unsigned int;
+    std::array<u32, 4> buffer{};
+    constexpr size_t buffer_size = sizeof(int) * buffer.size();
+    std::array<char, 64> charbuffer{};
+    constexpr std::array<u32, 3> ids = { 0x80000002, 0x80000003, 0x80000004 };
+    std::string brand = "";
+
+    if (CheckCpuid()) {
+        auto cpuid = [](u32& a, u32& b, u32& c, u32& d, const u32 a_leaf, const u32 c_leaf = 0xFF) {
+            int x[4];
+            __cpuidex(x, a_leaf, c_leaf);
+            a = static_cast<u32>(x[0]);
+            b = static_cast<u32>(x[1]);
+            c = static_cast<u32>(x[2]);
+            d = static_cast<u32>(x[3]);
             };
-
-        std::stringstream ss;
-
-        ss << strconvert(sig_reg[0]);
-        ss << strconvert(sig_reg[2]);
-        ss << strconvert(sig_reg[1]);
-
-        return ss.str();
-        };
-
-    // fetch cpu vendor 
-     auto vendor = [&]() -> std::string {
-        if (!check_cpuid()) {
-            return "";
-        }
-
-        std::array<u32, 4> buffer{};
-        constexpr std::size_t buffer_size = sizeof(i32) * buffer.size();
-        std::array<char, 64> charbuffer{};
-
-        constexpr std::array<u32, 3> ids = {
-            0x80000002,
-            0x80000003,
-            0x80000004
-        };
-
-        std::string brand = "";
 
         for (const u32& id : ids) {
             cpuid(buffer.at(0), buffer.at(1), buffer.at(2), buffer.at(3), id);
 
             std::memcpy(charbuffer.data(), buffer.data(), buffer_size);
-
             const char* convert = charbuffer.data();
             brand += convert;
         }
+    }
 
-        return brand;
-        };
+    return brand;
+}
 
-    // fetch thread count 
-     auto threadcount = []() -> u32 {
-        return std::thread::hardware_concurrency();
-        };
+// fetch thread count
+unsigned int GetThreadCount() {
+    return std::thread::hardware_concurrency();
+}
 
-    // fetch mac address
-     auto mac = []() -> u16 {
-        // C-style array on purpose
-        u8 mac[6] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+// fetch MAC address
+unsigned short GetMacAddress() {
+    unsigned char mac[6] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-        PIP_ADAPTER_INFO AdapterInfo;
-        DWORD dwBufLen = sizeof(IP_ADAPTER_INFO);
+    PIP_ADAPTER_INFO AdapterInfo;
+    DWORD dwBufLen = sizeof(IP_ADAPTER_INFO);
+    char* mac_addr = static_cast<char*>(std::malloc(18));
 
-        char* mac_addr = static_cast<char*>(std::malloc(18));
+    AdapterInfo = (PIP_ADAPTER_INFO)std::malloc(sizeof(IP_ADAPTER_INFO));
 
-        AdapterInfo = (IP_ADAPTER_INFO*)std::malloc(sizeof(IP_ADAPTER_INFO));
+    if (AdapterInfo == NULL) {
+        free(mac_addr);
+        return 0;
+    }
 
-        if (AdapterInfo == NULL) {
-            free(mac_addr);
-            return false;
-        }
-
-        if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
-            std::free(AdapterInfo);
-            AdapterInfo = (IP_ADAPTER_INFO*)std::malloc(dwBufLen);
-            if (AdapterInfo == NULL) {
-                std::free(mac_addr);
-                return false;
-            }
-        }
-
-        if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
-            PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
-            for (std::size_t i = 0; i < 6; i++) {
-                mac[i] = pAdapterInfo->Address[i];
-            }
-        }
-
+    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
         std::free(AdapterInfo);
+        AdapterInfo = (PIP_ADAPTER_INFO)std::malloc(dwBufLen);
+        if (AdapterInfo == NULL) {
+            std::free(mac_addr);
+            return 0;
+        }
+    }
 
-        const u16 mac_hash = mac[0] + mac[1] + mac[2] + mac[3] + mac[4] + mac[5];
-        return mac_hash;
-        };
+    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
+        PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
+        for (size_t i = 0; i < 6; i++) {
+            mac[i] = pAdapterInfo->Address[i];
+        }
+    }
 
-      // bios data thingy idfk
-     auto bios_shit = []() -> u64 {
-         std::system("wmic bios get serialnumber > sn.txt");
-         wchar_t sn[16];
+    std::free(AdapterInfo);
 
-         FILE* fp;
-         if (fopen_s(&fp, "sn.txt", "r, ccs=UTF-8") == 0) {
-             fgetws(sn, 16, fp); // dummy read of first line
-             fgetws(sn, 16, fp); // now sn contains the 2nd line
+    unsigned short mac_hash = mac[0] + mac[1] + mac[2] + mac[3] + mac[4] + mac[5];
+    return mac_hash;
+}
 
-             fclose(fp);         // cleanup temp file
-             remove("sn.txt");
+// fetch BIOS serial number
+unsigned long long GetBiosSerial() {
+    std::system("wmic bios get serialnumber > sn.txt");
+    wchar_t sn[16];
 
-             return reinterpret_cast<u64>(sn); // it works
-         }
-         else {
-             // Handle the error in opening the file
-             return 0; // or some other appropriate value
-         }
-         };
+    FILE* fp;
+    if (fopen_s(&fp, "sn.txt", "r, ccs=UTF-8") == 0) {
+        fgetws(sn, 16, fp); // dummy read of the first line
+        fgetws(sn, 16, fp); // now sn contains the 2nd line
+        fclose(fp);         // cleanup temp file
+        remove("sn.txt");
+        return reinterpret_cast<unsigned long long>(sn); // it works
+    }
+    else {
+        // Handle the error in opening the file
+        return 0; // or some other appropriate value
+    }
+}
 
+// Main function to get HWID
+std::string GetHWID() {
+    std::string hwid = GetCpuBrand() + GetCpuVendor();
+    hwid += std::to_string(GetThreadCount());
+    hwid += std::to_string(GetMacAddress());
+    hwid += std::to_string(GetBiosSerial());
 
-    // TODO: ADD STAGE 2 HERE
+    // Truncate or hash the combined string to 20 characters if needed
+    if (hwid.length() > 20) {
+        hwid = hwid.substr(0, 20); // Truncate to 20 characters
+    }
+
+    return hwid;
 }
